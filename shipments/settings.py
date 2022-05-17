3610950e00869e0bcb4a451b4831dda1d537ba13
j.rafalska <j.rafalska@bankier.pl>
2022-05-17 22:51:43 +0200

    cors policy

--- shipments/settings.py
+++ shipments/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = config('CORS_ORIGIN_ALLOW_ALL', cast=bool)
+
+CORS_ORIGIN_WHITELIST = config(
+    'CORS_ORIGIN_WHITELIST', cast=lambda v: [s.strip() for s in v.split(',')]
+)
